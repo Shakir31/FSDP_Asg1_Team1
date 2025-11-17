5936b6044f8a67824b4eb2ce61d370c2463d44d1
Darien Yap <S10259228@connect.np.edu.sg>
2025-11-18 00:31:22 +0800

    converted code to react and edited css files

--- frontend/BevEat/src/App.jsx
+++ frontend/BevEat/src/App.jsx
@@ -6,6 +6,11 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import StallPage from "./components/StallPage";
 import MainLayout from "./components/MainLayout";
+import Product from "./components/Product";
+import StallPhotos from "./components/StallPhotos";
+import ProfilePage from "./components/ProfilePage";
+import Home from "./components/Home";
+import HawkerPage from "./components/HawkerPage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,7 +21,12 @@ function App() {
       <Route path="/register" element={<Signup />} />
       {/* <Route path="/home" element={<Home />} /> */}
       <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/hawkers" element={<HawkerPage />} />
         <Route path="/stalls/:id" element={<StallPage />} />
+        <Route path="/stalls/:id/photos" element={<StallPhotos />} />
+        <Route path="/menu-item/:itemId" element={<Product />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Route>
     </Routes>
   );
